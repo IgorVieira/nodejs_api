02277a348f6a15170e1f377c597cc1e3ceb87f8f
Igor Vieira <igorpereiraramosvieira@gmail.com>
2016-08-30 01:19:25 -0300

    use http for project

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import app from "./config/app"
-
+import http from "http"
 
 const port = app.get('port')
 
-
-app.listen(port, ()=>{
+http.createServer(app).listen(port, ()=>{
   console.log('server is running:'+ port)
 })
